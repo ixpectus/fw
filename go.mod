ce41bf65b1b11a16e9b80f6466665a53ff6287f1
Anton Petrosyan <anpetrosyan@ozon.travel>
2019-09-09 18:05:56 +0300

    add more methods

--- go.mod
+++ go.mod
@@ -5,5 +5,7 @@ go 1.12
 require (
 	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
 	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/moul/http2curl v1.0.0
+	github.com/smartystreets/goconvey v0.0.0-20190731233626-505e41936337 // indirect
 	github.com/wtertius/pp v3.1.0+incompatible
 )
--- write.go
+++ write.go
@@ -1,12 +1,65 @@
 package fw
 
 import (
+	"fmt"
+	"net/http"
 	"os"
 
+	"strconv"
+
+	"github.com/moul/http2curl"
 	"github.com/wtertius/pp"
 )
 
+func WriteStructByMask(root, path string, v interface{}) error {
+	pp.ColoringEnabled = false
+	return WriteNewFileByMask(root, path, []byte(pp.Sprint(v)))
+}
+
+func WriteCurlReqByMask(root, path string, req *http.Request) error {
+	return WriteNewFileByMask(root, path, []byte(RequestToCurl(req)))
+}
+
+func RequestToCurl(req *http.Request) (curl string) {
+	command, _ := http2curl.GetCurlCommand(req)
+	return command.String()
+}
+
+func WriteNewFileByMask(root, path string, bb []byte) error {
+	if freeFileName, err := findFileByMask(root, path); err != nil {
+		fmt.Printf(">>> %s <<< debug\n", err)
+		return err
+	} else {
+		fmt.Printf(">>> %s <<< debug\n", freeFileName)
+
+		return Write(freeFileName, bb)
+	}
+}
+
+func findFileByMask(root, path string) (string, error) {
+	if _, err := os.Stat(root); os.IsNotExist(err) {
+		return "", fmt.Errorf("root directory not exists %s", root)
+	}
+	freeNameFound := false
+	i := 0
+	for !freeNameFound {
+		var freeName string
+		if i == 0 {
+			freeName = root + "/" + path
+		} else {
+			freeName = root + "/" + path + strconv.Itoa(i)
+		}
+		if _, err := os.Stat(freeName); os.IsNotExist(err) {
+			return freeName, nil
+		}
+		i++
+	}
+	return "", fmt.Errorf("unexpected error")
+}
+
 func Write(fileName string, bb []byte) error {
+	fmt.Printf("fw debug >>> %s <<< debug\n", fileName)
+
 	f, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		return err
